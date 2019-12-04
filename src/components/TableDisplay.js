8dd778349cd9685dc80e5b8eecffe05cf0ced1f7
Cedric Theofanous <cedrictheo@gmail.com>
2019-12-03 21:13:31 -0500

    complete re-render

--- src/components/TableDisplay.js
+++ src/components/TableDisplay.js
@@ -3,27 +3,34 @@ import Row from './Rows.js';
 import TableHeader from './TableHeader.js';
 
 class TableDisplay extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {}
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
-  render(){
-      const lengthRow = this.props.data.length;
-      const rowsArr = [];
-      
-      for(let i = 0; i < lengthRow; i += 1) {
-          rowsArr.push(<Row key={i + '_row'} tableName={this.props.tableName} uri={this.props.uri} data={this.props.data[i]} />)
-      }
-    
-    return(
+  render() {
+    const lengthRow = this.props.data.length;
+    const rowsArr = [];
+
+    for (let i = 0; i < lengthRow; i += 1) {
+      rowsArr.push(
+        <Row
+          key={i + '_row'}
+          tableName={this.props.tableName}
+          uri={this.props.uri}
+          data={this.props.data[i]}
+          reRender={this.props.reRender}
+        />
+      );
+    }
+
+    return (
       <div>
-        <TableHeader keys={Object.keys(this.props.data[0])}/>
+        <TableHeader keys={Object.keys(this.props.data[0])} />
         {rowsArr}
       </div>
-    )
+    );
   }
-
 }
 
 export default TableDisplay;
